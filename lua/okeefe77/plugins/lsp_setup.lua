f9ba165e7d36051119789213ead49ddadb117282
Seamus O'Keefe <okeefe77@gmail.com>
2024-07-07 17:36:46 -0400

    Initial LSP setup, lua_ls only, minimal keybinds

---
+++ lua/okeefe77/plugins/lsp_setup.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = true
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = { ensure_installed = {"lua_ls"} }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    init = function ()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set({ 'n', 'v' }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
--- lua/okeefe77/plugins/telescope.lua
+++ lua/okeefe77/plugins/telescope.lua
@@ -1,11 +1,25 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = {'nvim-lua/plenary.nvim'},
-  init = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-  end
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim'
+    },
+    opts = {
+      extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+      }
+    },
+    init = function()
+      require("telescope").load_extension("ui-select")
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  }
 }
