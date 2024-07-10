3420a543aa594abc9213ea68c749dc660c378953
Seamus O'Keefe <okeefe77@gmail.com>
2024-07-09 22:10:07 -0400

    Added clangd to LSP setup

--- lua/okeefe77/plugins/lsp_setup.lua
+++ lua/okeefe77/plugins/lsp_setup.lua
@@ -5,15 +5,17 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = { ensure_installed = {"lua_ls"} }
+    opts = { ensure_installed = { "lua_ls", "clangd" } }
   },
   {
     "neovim/nvim-lspconfig",
     init = function ()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, "<leader>ca", vim.lsp.buf.code_action, {})
     end
   }
