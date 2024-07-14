ef65529a92ec679e92f794db69009ff726e6c407
Seamus O'Keefe <okeefe77@gmail.com>
2024-07-14 15:13:35 -0400

    Added formatting with none-ls and configured for lua and clang. Set up format on save

--- lua/okeefe77/plugins/lsp_setup.lua
+++ lua/okeefe77/plugins/lsp_setup.lua
@@ -11,12 +11,15 @@ return {
     "neovim/nvim-lspconfig",
     init = function ()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, "<leader>ca", vim.lsp.buf.code_action, {})
+      local on_attach = function(client, bufnr)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set({ 'n', 'v' }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end
+
+      lspconfig.lua_ls.setup({ on_attach = on_attach })
+      lspconfig.clangd.setup({ on_attach = on_attach })
     end
   }
 }
